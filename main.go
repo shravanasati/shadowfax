7c00519cf4b3fa165429222bbf011aaca0ad4eaf
Shravan Asati <dev.shravan@protonmail.com>
2025-09-10 10:52:11 +0530

    get lines channel

--- main.go
+++ main.go
@@ -1,28 +1,57 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	go func ()  {
+		buffer := make([]byte, 8)
+		eof := false
+		part := ""
+		for !eof {
+			n, err := f.Read(buffer)
+			if err == io.EOF {
+				eof = true
+				continue
+			}
+			if err != nil {
+				panic(err)
+			}
+	
+			newlinePos := bytes.IndexByte(buffer[:n], '\n')
+			if newlinePos != -1 {
+				// found a newline
+				part += string(buffer[:newlinePos])
+				ch <- part
+				part = string(buffer[newlinePos+1 : n])
+			} else {
+				part += string(buffer[:n])
+			}
+		}
+		if part != "" {
+			ch <- part
+		}	
+
+		close(ch)
+	}()
+
+	return ch
+}
 
 func main() {
 	f, err := os.Open("messages.txt")
 	if err != nil {
 		panic(err)
 	}
+	defer f.Close()
 
-	buffer := make([]byte, 8)
-	eof := false
-	for !eof {
-		_, err := f.Read(buffer)
-		if err == io.EOF {
-			eof = true
-			continue
-		}
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("read: %s\n", buffer)
+	ch := getLinesChannel(f)
+	for item := range ch {
+		fmt.Printf("read: %s\n", item)
 	}
 }
