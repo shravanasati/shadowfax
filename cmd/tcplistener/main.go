1d214b9a7804fbcf268e9eb6c1ff07f6fe36fe29
Shravan Asati <dev.shravan@protonmail.com>
2025-09-12 23:35:03 +0530

    finish request line parsing, integrate in tcp listener

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,47 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"net"
+
+	"github.com/shravanasati/shadowfax/internal/request"
 )
-
-func getLinesChannel(f io.ReadCloser, bufSize int, sep []byte) <-chan string {
-	ch := make(chan string)
-	go func ()  {
-		buffer := make([]byte, bufSize)
-		eof := false
-		part := ""
-		for !eof {
-			n, err := f.Read(buffer)
-			if err == io.EOF {
-				eof = true
-				continue
-			}
-			if err != nil {
-				panic(err)
-			}
-	
-			sepPos := bytes.Index(buffer[:n], sep)
-			if sepPos != -1 {
-				// found a newline
-				part += string(buffer[:sepPos])
-				ch <- part
-				part = string(buffer[sepPos+len(sep) : n])
-			} else {
-				part += string(buffer[:n])
-			}
-		}
-		if part != "" {
-			ch <- part
-		}	
-
-		close(ch)
-	}()
-
-	return ch
-}
 
 func main() {
 	listener, err := net.Listen("tcp", ":42069")
@@ -57,9 +21,11 @@ func main() {
 			panic(err)
 		}
 		fmt.Println("a connection has been accepted", conn.RemoteAddr().String())
-		lines := getLinesChannel(conn, 8, []byte("\n"))
-		for line := range lines {
-			fmt.Println(line)
+		req, err := request.RequestFromReader(conn)
+		if err != nil {
+			fmt.Println("error parsing the request:", err)
+		} else {
+			fmt.Printf("Request line: \n\t- Method: %s \n\t- Target: %s \n\t- HTTP Version: %s \n", req.RequestLine.Method, req.RequestLine.Target, req.RequestLine.HTTPVersion)
 		}
 		fmt.Println("a connection has been closed")
 	}
